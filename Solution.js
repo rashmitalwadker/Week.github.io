70a38b91c32fc14c623dc8a46f5e438e595f55a6
rashmitalwadker <rashmitalwadker@gmail.com>
2021-04-11 16:15:30 +0530

    this is the typescript app

---
+++ Solution.js
@@ -0,0 +1,5 @@
+var d = new Array('2021/3/4,2021/3/5,2021/3/6,2021/3/7');
+function solution(d, b) {
+    return d + 0;
+}
+console.log(d[0], d[1], d[2], d[3]);
---
+++ Solution.ts
@@ -0,0 +1,6 @@
+var d=new Array('2021/3/4,2021/3/5,2021/3/6,2021/3/7');
+function solution(d:string,b:any){
+return d+b;
+
+}
+console.log(d[0],d[1],d[2],d[3]);
